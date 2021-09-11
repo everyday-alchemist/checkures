89ce507aa63c325d2a2c4ef2ccdb53ef2cdfaa65
Matthew Ciraula <ciraulam@tutanota.com>
2021-09-11 16:00:27 -0400

    almost done implementing basic logic

--- src/cljs/checkures/utils.cljs
+++ src/cljs/checkures/utils.cljs
@@ -76,34 +76,60 @@
   [adj from to]
   (contains? adj #{from to}))
 
-;(defn occupied?
-  ;[board row col]
-  
+(defn occupied?
+  [board row col]
+  (not= (get-2d board row col) :none))
 
-(defn dir
+(defn get-dir
+  ; TODO refactor to only take col?
   "returns the direction of a move from arg1 to arg2"
   [[_ y1] [_ y2]]
   (if (< y1 y2)
     :down
     :up))
 
+(defn get-valid-dirs
+  [piece]
+  (case piece
+    :red #{:up}
+    :black #{:down}
+    #{:up :down}))
+
+
+(defn valid-move?
+  [board [from-row from-col] [to-row to-col]]
+  (let [from-piece (get-2d board from-row from-col)
+        to-piece (get-2d board to-row to-col)
+        dir (get-dir [from-row from-col] [to-row to-col])
+        valid-dirs (get-valid-dirs from-piece)
+        dist (- from-col to-col)]
+    (and 
+      (not= from-piece :none)
+      (= to-piece :none)
+      (conn? board [from-row from-col] [to-row to-col])
+      (contains? valid-dirs dir)
+      (or (= dist 1) (= dist -1)))))
+
 (defn even-row
   "turn even rows into html"
-  [row]
-  (into [:tr]
+  [i row]
+  (let [index (* i 2)]
+  (into [:tr {:row index}]
         (interleave (repeat 4 [:td {:class :unplayable}])
-                    (map #(vec [:td (assoc {} :class %)]) row))))
-
+                    (map-indexed #(vec [:td (assoc {} :class %2 :row index :col %1)]) row)))))
 (defn odd-row
   "turn odd rows into html"
-  [row]
-  (into [:tr]
-        (interleave (map #(vec [:td (assoc {} :class %)]) row)
-                    (repeat 4 [:td {:class :unplayable}]))))
+  [i row]
+  (let [index (inc (* i 2))]
+  (into [:tr {:row index}]
+        (interleave (map-indexed #(vec [:td (assoc {} :class %2 :row index :col %1)]) row)
+                    (repeat 4 [:td {:class :unplayable}])))))
 
 (defn show-board
   [board]
   (into [:table]
         (interleave
-         (map #(even-row %) (take-nth 2 board))
-         (map #(odd-row %) (take-nth 2 (rest board))))))
+         (map-indexed even-row (take-nth 2 board))
+         (map-indexed odd-row (take-nth 2 (rest board))))))
+
+;; TODO: fix the col/row x/y situation
